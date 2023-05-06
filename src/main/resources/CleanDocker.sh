8de4b1db844944e4299774a77bae59369d5feb72
wean <wuwg@agilewing.net>
2023-05-06 14:11:25 +0800

    镜像脚本更新

--- src/main/resources/CleanDocker.sh
+++ src/main/resources/CleanDocker.sh
@@ -1,16 +1,16 @@
 #!/bin/
-imagename=10.0.12.15/dev-test/demo:latest
+imagename=10.0.12.15/dev-test/demo
 docker images | grep $imagename &> /dev/null
 #如果存在，删除该镜像
 if [ $? -eq 0 ]
 then
-    echo $$imagename" image is existed,we will remove it!!!"
+    echo $imagename" image is existed,we will remove it!!!"
     docker rmi $(docker images | grep $imagename | awk "{print $3}")
 fi
 
 
-sudo docker build -t $imagename ./classes
+sudo docker build -t $imagename:latest ./classes
 
-sudo docker tag $imagename $imagename
+sudo docker tag $imagename:latest $imagename:latest
 
-sudo docker push $imagename
+sudo docker push $imagename:latest
